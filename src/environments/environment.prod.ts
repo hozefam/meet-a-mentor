a62f626bdeffc3067be861f6da9a05cb03b4002e
Hozefa Mangalorewala <hozefam@live.in>
2022-04-16 01:05:47 +0300

    updated url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   APP_ENDPOINT:
-    'http://8080-appwrite-integrationforg-8975o1yd3lc.ws-eu40.gitpod.io/v1',
+    'https://8080-appwrite-integrationforg-8975o1yd3lc.ws-eu40.gitpod.io/v1',
   APP_PROJECT: '6259eb59da2523b0f862',
   APP_COLLECTION_ID: '',
   MAXDAYSLIMIT: 3,
